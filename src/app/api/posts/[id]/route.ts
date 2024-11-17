90a41ea93adb848bdca2913883988f0cc1fa5c3e
Kazem Ramadan <kazem@thndr.app>
2024-11-18 01:56:05 +0200

    another fix

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,12 +1,12 @@
 import { getPostbyID } from "@/lib/actions/post.actions";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-    const { id } = params;
+export async function GET(req: NextRequest, context: { params: { id: string } }) {
+    const { id } = context.params;  // Accessing params via context
     try {
         const post = await getPostbyID(id);
-        return new Response(JSON.stringify(post), { status: 200 });
-    } catch {
-        return new Response(JSON.stringify({ error: "Internal Error" }), { status: 500 });
+        return NextResponse.json(post);  // Using NextResponse to return a JSON response
+    } catch (error) {
+        return NextResponse.json({ error: `Internal Error ${error}` }, { status: 500 });
     }
 }
