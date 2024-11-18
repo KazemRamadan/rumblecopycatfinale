2aab1a16870c0a0e40c8f8f019cc9b1dea510c2f
Kazem Ramadan <kazem@thndr.app>
2024-11-18 02:18:14 +0200

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+import { NextRequest } from "next/server";
 
 export default withAuth(
-    async function middleware() {
+    async function middleware(request: NextRequest) {
+        console.log(request)
     }, {
         isReturnToCurrentPage: true,
     }
